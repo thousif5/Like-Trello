f81cc57ccd296fd6ac0ac4bf79f2c521c126b262
Mohammed Thousif Shaik <mohammed.thousif@mountblue.io>
2019-03-29 18:02:39 +0530

    changed variable names to more descriptive way

--- index.js
+++ index.js
@@ -5,115 +5,137 @@ var cardID = "5c976eebb77e4583fc7609f5";
 var developerKey = "89054c5990edbc128a3b8e87fb053290";
 var developerToken = "1c788cb9754bd3aef0f81f69c1418c4522a114cf5aada05f02eb2d5e04b3a1e7";
 
+function getData() {
+  fetch(`https://api.trello.com/1/cards/${cardID}/checklists?key=${developerKey}&token=${developerToken}`)
+    .then(response => {
+      return response.json();
+    }).then(checklistsData => {
+      let cardData = checklistsData;
+      return toPopulateData(cardData);
+    }).catch(err => {
+      console.log(err)
+    })
+}
+
 $(document).ready(function () {
-
-    getData();
-
-    function getData() {
-        fetch(`https://api.trello.com/1/cards/${cardID}/checklists?key=${developerKey}&token=${developerToken}`)
-            .then(response => {
-                if (response.status != 200) {
-                    alert("You're offline");
-                }
-                return response.json();
-            }).then(checklistsData => {
-                let cardData = checklistsData;
-                return toOperate(cardData);
-            }).catch(err => {
-
-                console.log(err);
-            })
-    }
-
-
-
+  getData();
 });
 
-// code for Operations
+// code for populating data from API
+function toPopulateData(cardData) {
+
+  let htmlForChecklistData =
+    `<div class = "divForChecklist">
+          <h3>${cardData[0]['name']}</h3>
+          <div class = "divForInput">
+             <input class="inputForItems" placeholder = "Add Item">
+             <button class = "itemButton">Add Items</button>
+          </div>
+       </div>`;
+  $(".content-div").append(htmlForChecklistData);
 
 
-function toOperate(cardData) {
+  cardData[0]["checkItems"].forEach(item => {
 
-    let newDiv = `<div class = "newDivFor"><h3>${cardData[0]['name']}</h3><div class = "newDivForFlex"><input class="inputForItems" placeholder = "Add Item"><button class = "buttonItem">Add Items</button></div></div>`;
-    $(".content-div").append(newDiv);
+    let createNewHtmlForCheckItems =
+      `<div class = "divByApi" id = "${item['id']}">
+            <div>
+                <input type="checkbox" class = "checkbox">
+            </div>
+            <div class = "divForCheckItem">
+                <p>${item["name"]}</p>
+            </div>
+            <div>
+                <button class = "divForDelete">Delete Item</button>
+            </div>
+        </div>`
 
+    $(".divForChecklist").append(createNewHtmlForCheckItems);
 
-    cardData[0]["checkItems"].forEach(item => {
+    let checkboxForCheckItems = document.querySelector(".checkbox");
+    if (item["state"] === "complete") {
+      checkboxForCheckItems.checked = true;
+    } else {
+      checkboxForCheckItems.checked = false;
+    }
 
-        let creDiv = `<div class = "divByApi" id = "${item['id']}"><div><input type="checkbox" class = "checkboxForP"></div><div class = "classForCheckItem"><p>${item["name"]}</p></div><div><button class = "divDelete">Delete Item</button></div></div>`
-
-        $(".newDivFor").append(creDiv);
-
-        let paraCB = document.querySelector(".checkboxForP");
-        if (item["state"] === "complete") {
-            paraCB.checked = true;
-        } else {
-            paraCB.checked = false;
-        }
-        $(".content-div").click(function (e) {
-            if (e.target.className === undefined) return;
-            if (e.target.className === "divDelete") deleteItems(`${cardData[0]['id']}`, `${item['id']}`);
-            if (e.target.className === "buttonItem") AddItemButton(`${cardData[0]["id"]}`);
-            if (e.target.className === "checkboxForP") checkboxStateUpdate(item["id"]);
-        })
-
-    })
+  })
 
 }
 
-// for updating checkbox
+// on-click function
+$(".content-div").click(function (e) {
+  if (e.target.className === "divForDelete") deleteItems(`${e.target.parentElement.parentElement.id}`);
+  if (e.target.className === "itemButton") AddItemButton();
+  if (e.target.className === "checkbox") checkboxStateUpdate(`${e.target.parentElement.parentElement.id}`);
+})
 
+
+// for updating checkbox
 function checkboxStateUpdate(id) {
 
-    let getCheckboxElement = document.getElementById(id)
-    let stateCheck = getCheckboxElement.querySelector('input')
-    let state;
+  let getCheckboxElement = document.getElementById(id)
+  let stateCheck = getCheckboxElement.querySelector('input')
+  let state;
 
-    if (stateCheck.checked) {
-        state = 'complete'
-    } else {
-        state = 'incomplete'
-    }
+  if (stateCheck.checked) {
+    state = 'complete'
+  } else {
+    state = 'incomplete'
+  }
 
-    fetch(`https://api.trello.com/1/cards/${cardID}/checkItem/${id}?state=${state}&key=${developerKey}&token=${developerToken}`, {
-        method: "PUT",
-    }).catch(err => {
-        console.log(err);
-    })
+  fetch(`https://api.trello.com/1/cards/${cardID}/checkItem/${id}?state=${state}&key=${developerKey}&token=${developerToken}`, {
+    method: "PUT",
+  }).catch(err => {
+    //stateCheck.checked = false;
+    stateCheck.disabled = true;
+    alert("You're Offline!");
+  })
 }
 
 // for adding checkItems
+function AddItemButton() {
 
-function AddItemButton(id) {
+  let inputForCheckItems = document.querySelector('.inputForItems').value;
+  if (inputForCheckItems == "") {
+    inputForCheckItems = "Item";
+  }
 
-    let inputVar = document.querySelector('.inputForItems').value;
-    if (inputVar == "") {
-        inputVar = "Item";
+  let htmlToPopulateData =
+    `<div class="divByApi" id = "">
+        <div>
+            <input type="checkbox" class="checkbox">
+        </div>newItem
+        newItem
+        <div class="divForCheckItem">
+            <p>${inputForCheckItems}</p>
+        </div>
+        <div>
+            <button class="divForDelete">Delete Item</button>
+        </div>
+     </div>`;
+
+  fetch(`https://api.trello.com/1/checklists/5c9c47c9a0bcac2460441862/checkItems?name=${inputForCheckItems}&pos=bottom&checked=false&key=${developerKey}&token=${developerToken}`, {
+    method: "POST",
+  }).then(response => {
+    if (response.status === 200) {
+      $(".divForChecklist").append(htmlToPopulateData);
     }
-
-    let newItem = `<div class="divByApi" id = ""><div><input type="checkbox" class="checkboxForP"></div><div class="classForCheckItem"><p>${inputVar}</p></div><div><button class="divDelete">Delete Item</button></div></div>`
-    $(".newDivFor").append(newItem);
-
-    fetch(`https://api.trello.com/1/checklists/${id}/checkItems?name=${inputVar}&pos=bottom&checked=false&key=${developerKey}&token=${developerToken}`, {
-        method: "POST",
-    }).then(response => {
-        return response.json();
-    }).catch(err => {
-        console.log(err);
-    })
+  }).catch(err => {
+    alert("You're Offline!")
+  })
 }
 
 // for deleting items in checklist
+function deleteItems(checkItemId) {
 
-function deleteItems(checklistId, checkItemId) {
-    //console.log("Delete Items", checkItemId);
-
-    //$(`#${checkItemId}`).remove();
-    document.getElementById(`${checkItemId}`).remove();
-
-    fetch(`https://api.trello.com/1/checklists/${checklistId}/checkItems/${checkItemId}?key=${developerKey}&token=${developerToken}`, {
-        method: "DELETE",
-    }).catch(err => {
-        console.log(err);
-    })
+  fetch(`https://api.trello.com/1/checklists/5c9c47c9a0bcac2460441862/checkItems/${checkItemId}?key=${developerKey}&token=${developerToken}`, {
+    method: "DELETE",
+  }).then(response => {
+    if (response.status === 200) {
+      document.getElementById(`${checkItemId}`).remove();
+    }
+  }).catch(err => {
+    alert("You're Offline!")
+  })
 }
